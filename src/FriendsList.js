e51e12f73b34c7083b3135fa29cf1d804e3a345c
Lucie Bučková <lucie.buckova@gmail.com>
2024-01-19 17:39:00 +0100

    Add missing <ul> tags and edit readme file

--- src/FriendsList.js
+++ src/FriendsList.js
@@ -1,14 +1,18 @@
 import Friend from './Friend';
 
 export default function FriendsList({ friends, onSelection, selectedFriend }) {
-  return friends.map((friend) => (
-    <Friend
-      key={friend.id}
-      name={friend.name}
-      image={friend.image}
-      balance={friend.balance}
-      onSelection={onSelection}
-      selectedFriend={selectedFriend}
-    />
-  ));
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend
+          key={friend.id}
+          name={friend.name}
+          image={friend.image}
+          balance={friend.balance}
+          onSelection={onSelection}
+          selectedFriend={selectedFriend}
+        />
+      ))}
+    </ul>
+  );
 }
